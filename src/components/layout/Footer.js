519586a96cabc4b40bf8ced89e6a8cdbb60f5461
danielkbr <danielkbr89@gmail.com>
2023-04-07 22:39:18 -0300

    testing react-icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,21 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer () {
   return (
     <footer>
-      Rodapé
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook/>
+        </li>
+        <li>
+          <FaInstagram/>
+        </li>
+        <li>
+          <FaLinkedin/>
+        </li>
+      </ul>
+      <p>Nosso rodapé</p>
     </footer>
   )
 }
